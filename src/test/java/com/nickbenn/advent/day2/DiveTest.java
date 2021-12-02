3d5fda9d12562c667a30c654c5ce89e6eb5acf94
Nick Bennett <nick@nickbenn.com>
2021-12-02 00:03:22 -0700

    Add unit tests for days 1 & 2.

---
+++ src/test/java/com/nickbenn/advent/day2/DiveTest.java
@@ -0,0 +1,39 @@
+/*
+ *  Copyright 2021 Nicholas Bennett.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.nickbenn.advent.day2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.nickbenn.advent.util.Defaults;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import org.junit.jupiter.api.Test;
+
+class DiveTest {
+
+  @Test
+  void positionDotProduct() throws URISyntaxException, IOException {
+    Dive dive = new Dive(Defaults.TEST_FILENAME);
+    assertEquals(150, dive.positionDotProduct());
+  }
+
+  @Test
+  void positionDotProductAim() throws URISyntaxException, IOException {
+    Dive dive = new Dive(Defaults.TEST_FILENAME);
+    assertEquals(900, dive.positionDotProductAim());
+  }
+
+}
